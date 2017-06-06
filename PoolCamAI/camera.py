156a68e15e7c8acb2a483c3a9433334ba05feb38
NiobeCipher <jettcalder@gmail.com>
2017-06-06 20:50:57 +0700

    add snapshot

--- PoolCamAI/camera.py
+++ PoolCamAI/camera.py
@@ -28,8 +28,49 @@ class VideoCamera(object):
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
+
+class SnapShot(object):
+    def __init__(self):
+        # Camera 0 is the integrated web cam on my netbook
+        camera_port = 'rtsp://admin:admin@10.0.17.13:80/'
+
+        # Number of frames to throw away while the camera adjusts to light levels
+        # ramp_frames = 30
+
+        # Now we can initialize the camera capture object with the cv2.VideoCapture class.
+        # All it needs is the index to a camera port.
+        self.camera = cv2.VideoCapture(camera_port)
+
+
+    # Captures a single image from the camera and returns it in PIL format
+    def get_image(self):
+        cap = SnapShot()
+        # read is the easiest way to get a full image out of a VideoCapture object.
+        retval, im = cap.camera.read()
+        return im
+
     def get_shot(self):
-        # Getting a snapshot
-        # Read current frame
-        success, image = self.video.read()
-        self.video.imwrite('Snapshot_{0}'.format(str(int(time()))), image)
+        temp = SnapShot()
+        # temp.get_image()
+        # Ramp the camera - these frames will be discarded and are only used to allow v4l2
+        # to adjust light levels, if necessary
+        for i in range(30):
+            temp.get_image()
+        print("Taking image...")
+        # Take the actual image we want to keep
+        camera_capture = self.get_image()
+        file = "test_image.png"
+        # A nice feature of the imwrite method is that it will automatically choose the
+        # correct format based on the file extension you provide. Convenient!
+        cv2.imwrite(file, camera_capture)
+        print("success")
+        view = cv2.imread('/test_image.png',1)
+        cv2.imshow('Preview', view)
+        cv2.waitKey(1000)
+        # You'll want to release the camera, otherwise you won't be able to create a new
+        # capture object until your script exits
+        del self.camera
+
+if __name__ == '__main__':
+    x = SnapShot()
+    x.get_shot()
