a430c6072335489552d83371f3b21a3d02b9ed40
monyoudom <thongmonyoudom@outlook.com>
2017-06-08 19:38:43 +0700

    git inor .pyc

--- ProjectAI/urls.py
+++ ProjectAI/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', include('PoolCamAI.urls')),
+    url(r'^', include('PoolCamAI.urls')),
     url(r'^cam/', include('PoolCamAI.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
