dfb7647ee827028792fb2e88e0c5b5c0364c0d8e
NiobeCipher <jettcalder@gmail.com>
2017-06-20 16:15:28 +0700

    Documented camera.py

--- PoolCamAI/camera.py
+++ PoolCamAI/camera.py
@@ -13,8 +13,7 @@ from PoolCamAI.detection import detection
 
 class VideoCamera(object):
 	def __init__(self):
-		camera = 'rtsp://admin:admin@10.0.17.15:80/live'
-
+		camera = 'rtsp://admin:admin@10.0.17.13:80/live'
 		#######################################################################
 		# Camera Options                                                      #
 		# My phone IP Cam - 'http://10.10.10.149:8080/video'                  #
@@ -26,47 +25,65 @@ class VideoCamera(object):
 		# Using OpenCV to capture from device 0. If you have trouble capturing
 		# from a webcam, comment the line below out and use a video file
 		# instead.
-
-		# self.video = cv2.VideoCapture('http://192.168.1.106:8080/video')
-		self.vs = WebcamVideoStream(src=camera).start()
 		# If you decide to use video.mp4, you must have this file in the folder
 		# as the main.py.
-		# self.video = cv2.VideoCapture('video.mp4')
-		# initialize the HOG descriptor/person detector
+
+		# Initialize camera threading and start getting image object to self.vs variable
+		self.vs = WebcamVideoStream(src=camera).start()
+		# Getting image width and height
 		self.W = self.vs.getW()
 		self.H = self.vs.getH()
+		# Initialize detection program and specify height and width
 		self.detect = ConDetect(self.H, self.W)
-		# print('Hi')
+
 
 	def __del__(self):
 		# self.video.release()
 		self.vs.stop()
 
 	def get_frame(self):
-
-		# faceCascade = cv2.CascadeClassifier('D:\AI\ProjectAI\media\haarcascade_fullbody.xml')
-		# success, image = self.video.read()
+		# Reading image from camera using threading technique
 		image = self.vs.read()
-		# data = detection(image)
-		data = self.detect.detect(image)
-		if not data['jpeg'] is None:
-			return data['jpeg'].tobytes(), data['num'], data['personflag']
-		else:
-			return image.tobytes()
 
+		# # Detecting then streaming
+		# data = self.detect.detect(image)
+		# if not data['jpeg'] is None:
+		# 	return data['jpeg'].tobytes(), data['num'], data['personflag']
+		# else:
+		# 	return image.tobytes()
+
+		# Streaming from webcam directly
+		ret, jpeg = cv2.imencode('.jpg', image)
+		return jpeg.tobytes(), 0, False
+
+	# No longer used. Reason: Too slow and have redundancy.
+	# Keep it in case user want to take snapshot without detection border in frame
 	def get_shot(self):
-		file = "snap_{0}.jpg".format(strftime("%b_%d_%Y_%H_%M", gmtime()))
-		# ret, image = self.video.read()
+		# Naming snapshot file
+		filename = "snap_{0}.jpg".format(strftime("%b_%d_%Y_%H_%M", gmtime()))
+		# Reading image from camera using threading technique
 		image = self.vs.read()
-		flag = cv2.imwrite(file, image)
+		# Writing image to file with filename
+			# Return true on success and false otherwise
+		flag = cv2.imwrite(filename, image)
+		# Print to console for Dev
 		if flag:
-			print('snap saved', strftime("%c"))
+			print(filename, ' was saved successfully on ', strftime("%c"))
+		else:
+			print(filename, ' was saved unsuccessfully on ', strftime("%c"))
 
 	def get_shotauto(self, image):
-			file = "snap_{0}.jpg".format(strftime("%b_%d_%Y_%H_%M_%S", gmtime()))
+			# Naming snapshot file
+			filename = "snap_{0}.jpg".format(strftime("%b_%d_%Y_%H_%M_%S", gmtime()))
+			# Converting image from binary string format to array
 			image = np.asarray(bytearray(image), dtype=np.uint8)
+			# Decoding image with color
 			image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-			# print(image)
-			flag = cv2.imwrite(file, image)
+			# Writing image to file with filename
+				# Return true on success and false otherwise
+			flag = cv2.imwrite(filename, image)
+			# Print to console for Dev
 			if flag:
-				print('snap auto saved', strftime("%c"))
+				print(filename, ' was saved successfully on ', strftime("%c"))
+			else:
+				print(filename, ' was saved unsuccessfully on ', strftime("%c"))
